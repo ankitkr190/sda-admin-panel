1d6d7a95163b3f95602916021ec05e6470ba0f19
ankitkr190 <ankitgiri190@gmail.com>
2021-01-07 21:07:58 +0530

    admin-created

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.w5ukw.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'sda-admin-panel'),
+        username: env('DATABASE_USERNAME', 'Admin'),
+        password: env('DATABASE_PASSWORD', 'admin@123'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89f6a811-37c2-43b1-8314-4476d2912c4c'
+};
