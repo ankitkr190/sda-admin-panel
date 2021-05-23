2b0fe5ba7f9eb413fba2d21c5999891704e520a3
Ankit Kumar <ankitgiri190@gmail.com>
2021-05-23 22:32:30 +0530

    sms ui changed

--- plugins/sms-portal/admin/src/containers/HomePage/index.js
+++ plugins/sms-portal/admin/src/containers/HomePage/index.js
@@ -5,19 +5,43 @@
  */
 
 import React, { memo } from "react";
+import "../HomePage/style.css";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 style={divStyle}>Welcome to SDA-SMS-PORTAL</h1>
-      <a href="https://control.textlocal.in/">Send SMS</a>
+      <div>
+        <h1 className="header">Welcome to SDA-SMS-PORTAL</h1>
+      </div>
+
+      <div className="sms-portal">
+        <div className="column">
+          <div className="card">
+            <h1>Gym</h1>
+            <a href="https://control.textlocal.in/">Send SMS</a>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h1>Dance</h1>
+            <a href="https://control.textlocal.in/">Send SMS</a>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h1>Taekwondo</h1>
+            <a href="https://control.textlocal.in/">Send SMS</a>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h1>Skating</h1>
+            <a href="https://control.textlocal.in/">Send SMS</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
-
-const divStyle = {
-  color: "black",
-  textAlign: "center",
 };
 
 export default memo(HomePage);
